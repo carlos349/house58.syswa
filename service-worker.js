8dfbb70cd84a9abe55016c59b7157ecea5bad422
carlos capacho <carlos.gomes349@gmail.com>
2022-02-02 17:44:47 -0300

    make build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcff64f4838f5bbcae5c593a0325ef3b.js"
+  "/precache-manifest.41bf5b0d82170a7d17c7aef0d5c89198.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
