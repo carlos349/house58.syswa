8d493d6609d5b7b3683b74d2075c51411b3e1a99
carlos capacho <carlos.gomes349@gmail.com>
2022-02-02 18:36:17 -0300

    fixed change for this client

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41bf5b0d82170a7d17c7aef0d5c89198.js"
+  "/precache-manifest.6cb904e6717f96f13c5093c3b4705d9b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
